d2c1a14b473f884094e1ddaa99ff3f569d45549c
Goksel Koksal <gokselkoksal@gmail.com>
2016-11-19 16:44:25 +0300

    Initial commit

---
+++ Lightning/Lightning.h
@@ -0,0 +1,19 @@
+//
+//  Lightning.h
+//  Lightning
+//
+//  Created by Göksel Köksal on 19/11/2016.
+//  Copyright © 2016 GK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Lightning.
+FOUNDATION_EXPORT double LightningVersionNumber;
+
+//! Project version string for Lightning.
+FOUNDATION_EXPORT const unsigned char LightningVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Lightning/PublicHeader.h>
+
+
