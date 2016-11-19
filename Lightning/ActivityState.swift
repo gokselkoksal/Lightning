ae95818c9d5823037f7009e5a800316b9e9e8e87
Goksel Koksal <gokselkoksal@gmail.com>
2016-11-19 20:40:35 +0300

    Add activity state model

---
+++ Lightning/ActivityState.swift
@@ -0,0 +1,53 @@
+//
+//  ActivityState.swift
+//  Lightning
+//
+//  Created by Göksel Köksal on 19/11/2016.
+//  Copyright © 2016 GK. All rights reserved.
+//
+
+import Foundation
+
+/// Model that tracks live activities by counting them.
+public struct ActivityState {
+    
+    /// Possible `ActivityState` exceptions.
+    ///
+    /// - nothingToRemove: Thrown when `remove()` is called when `count` is 0.
+    public enum Exception: Error {
+        case nothingToRemove
+    }
+    
+    /// Count of live activities.
+    public private(set) var count: UInt = 0
+    
+    /// True if `isActive` flag is toggled recently.
+    public private(set) var isToggled = false
+    
+    /// Flag to indicate if there are live activities.
+    public var isActive: Bool {
+        return count > 0
+    }
+    
+    /// Adds a new activity to tracker.
+    public mutating func add() {
+        isToggled = (count == 0)
+        count += 1
+    }
+    
+    /// Removes an activity from tracker.
+    public mutating func remove() throws {
+        guard count > 0 else {
+            throw Exception.nothingToRemove
+        }
+        count -= 1
+        isToggled = (count == 0)
+    }
+}
+
+extension ActivityState: Equatable {
+    
+    public static func ==(left: ActivityState, right: ActivityState) -> Bool {
+        return left.isToggled == right.isToggled && left.count == right.count
+    }
+}
---
+++ LightningTests/ActivityStateTests.swift
@@ -0,0 +1,41 @@
+//
+//  ActivityStateTests.swift
+//  Lightning
+//
+//  Created by Göksel Köksal on 19/11/2016.
+//  Copyright © 2016 GK. All rights reserved.
+//
+
+import XCTest
+@testable import Lightning
+
+class ActivityStateTests: XCTestCase {
+    
+    func test() {
+        var loadingState = ActivityState()
+        loadingState.test(isActive: false, count: 0, isToggled: false)
+        loadingState.add()
+        loadingState.test(isActive: true, count: 1, isToggled: true)
+        loadingState.add()
+        loadingState.test(isActive: true, count: 2, isToggled: false)
+        try? loadingState.remove()
+        loadingState.test(isActive: true, count: 1, isToggled: false)
+        try? loadingState.remove()
+        loadingState.test(isActive: false, count: 0, isToggled: true)
+        do {
+            try loadingState.remove()
+        } catch let error as ActivityState.Exception {
+            XCTAssert(error == ActivityState.Exception.nothingToRemove)
+        } catch {
+            XCTFail("Cannot throw anything else.")
+        }
+    }
+}
+
+extension ActivityState {
+    func test(isActive: Bool, count: UInt, isToggled: Bool) {
+        XCTAssert(self.isActive == isActive)
+        XCTAssert(self.count == count)
+        XCTAssert(self.isToggled == isToggled)
+    }
+}
