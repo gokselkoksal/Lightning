af467cef76f149f506b8f9dd20f38ff6e21cc69b
Göksel Köksal <gokselkoksal@gmail.com>
2016-12-19 23:47:15 +0300

    Component: Add string formatter

---
+++ Lightning/Sources/Components/Formatter.swift
@@ -0,0 +1,16 @@
+//
+//  Formatter.swift
+//  Lightning
+//
+//  Created by Goksel Koksal on 19/12/2016.
+//  Copyright © 2016 GK. All rights reserved.
+//
+
+import Foundation
+
+protocol Formatter {
+    associatedtype Value
+    func format(_ value: Value) -> Value
+    func unformat(_ value: Value) -> Value
+    func isFormatted(_ value: Value) -> Bool
+}
