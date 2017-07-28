69c073bc32eab840cb5098b17e8f3681e0811381
Goksel Koksal <gokselkoksal@gmail.com>
2017-07-28 21:28:26 +0100

    Bundle: Deprecate buildNumberString and longVersionString

--- LightningTests/ExtensionTests/BundleHelpersTests.swift
+++ LightningTests/ExtensionTests/BundleHelpersTests.swift
@@ -14,10 +14,8 @@ class BundleHelpersTests: XCTestCase {
     func testVersionStrings() {
         let bundle = Bundle(for: BundleHelpersTests.self)
         let shortVersion = bundle.zap_shortVersionString
-        let buildVersion = bundle.zap_buildNumberString
-        let longVersion = bundle.zap_longVersionString
+        let buildVersion = bundle.zap_versionString
         XCTAssert(shortVersion == "1.0")
         XCTAssert(buildVersion == "1")
-        XCTAssert(longVersion == "\(shortVersion) (\(buildVersion))")
     }
 }
