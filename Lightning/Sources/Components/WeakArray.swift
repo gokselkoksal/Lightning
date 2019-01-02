f33aebbd24dafb75331d78599df8c94a11f35d6f
Göksel Köksal <gokselkoksal@gmail.com>
2019-01-02 21:58:38 +0300

    Improve Weak component and tests

--- Lightning/Sources/Components/WeakArray.swift
+++ Lightning/Sources/Components/WeakArray.swift
@@ -10,47 +10,59 @@ import Foundation
 
 /// Array to keep elements weakly.
 public struct WeakArray<Element: AnyObject> {
-    
-    /// Array of weak elements.
-    public private(set) var weakElements: [Weak<Element>] = []
-    
-    /// Array of non-nil elements.
-    public var elements: [Element] {
-        return weakElements.compactMap { $0.value }
-    }
-    
-    /// Creates a weak array with given elements.
-    ///
-    /// - Parameter elements: Initial elements.
-    public init(elements: [Element] = []) {
-        for element in elements {
-            appendWeak(element)
-        }
-    }
-    
-    /// Wraps given object in `Weak` struct and appends it to `weakElements`.
-    ///
-    /// - Parameter object: Object to append weakly.
-    public mutating func appendWeak(_ object: Element?) {
-        guard let object = object else { return }
-        weakElements.append(Weak(object))
-    }
-    
-    /// Removes all wrappers.
-    public mutating func removeAll() {
-        weakElements.removeAll()
-    }
-    
-    /// Removes wrappers with nil values from elements.
-    public mutating func compact() {
-        let indexesToDelete = NSMutableIndexSet()
-        for (index, weakObject) in weakElements.enumerated() {
-            if weakObject.value == nil {
-                indexesToDelete.add(index)
-            }
-        }
-        for index in indexesToDelete {
-            weakElements.remove(at: index)
-        }
-    }
+  
+  /// Array of weak elements.
+  public var weakElements: [Weak<Element>]
+  
+  /// Creates a weak array with given elements.
+  ///
+  /// - Parameter elements: Initial elements.
+  public init(elements: [Element] = []) {
+    weakElements = elements.map({ Weak($0) })
+  }
+  
+  /// Wraps given object in `Weak` struct and appends it to `weakElements`.
+  ///
+  /// - Parameter object: Object to append weakly.
+  public mutating func append(_ object: Element?) {
+    guard let object = object else { return }
+    weakElements.append(Weak(object))
+  }
+  
+  /// Removes wrappers with nil values from elements.
+  public mutating func compact() {
+    weakElements = weakElements.filter({ $0.value != nil })
+  }
+  
+  /// Reads elements stored in weak array.
+  ///
+  /// - Returns: Strong references to the elements.
+  public func strongElements() -> [Element] {
+    return weakElements.compactMap({ $0.value })
+  }
+}
+
+// MARK: - Deprecated
+
+public extension WeakArray {
+  
+  /// Array of non-nil elements.
+  @available(*, deprecated, message: "Use `strongElements()` instead.")
+  public var elements: [Element] {
+    return strongElements()
+  }
+  
+  /// Removes all wrappers.
+  @available(*, deprecated, message: "Use `weakElements.removeAll()` instead. Will be removed soon.")
+  public mutating func removeAll() {
+    weakElements.removeAll()
+  }
+  
+  /// Wraps given object in `Weak` struct and appends it to `weakElements`.
+  ///
+  /// - Parameter object: Object to append weakly.
+  @available(*, deprecated, message: "Use `append(...)` instead. Will be removed soon.")
+  public mutating func appendWeak(_ object: Element?) {
+    append(object)
+  }
 }
--- LightningTests/ComponentTests/WeakTests.swift
+++ LightningTests/ComponentTests/WeakTests.swift
@@ -10,54 +10,60 @@ import XCTest
 @testable import Lightning
 
 class WeakTests: XCTestCase {
+  
+  fileprivate final class Ref {
+    var name: String
     
-    private final class Ref: Equatable {
-        var name: String
-        
-        init(name: String) {
-            self.name = name
-        }
-        
-        static func ==(a: Ref, b: Ref) -> Bool {
-            return a.name == b.name
-        }
+    init(name: String) {
+      self.name = name
     }
+  }
+  
+  func testExample() {
+    // Note: NSString is most probably inferred as String at some point.
+    // It will not work with Weak struct.
     
-    func testExample() {
-        // Note: NSString is most probably inferred as String at some point.
-        // It will not work with Weak struct.
-        
-        var ref1: Ref? = Ref(name: "str1")
-        var ref2: Ref? = Ref(name: "str2")
-        var ref3: Ref? = Ref(name: "str3")
-        var array = WeakArray<Ref>()
-        
-        array.test(weakCount: 0, nonNilCount: 0, elements: [])
-        array.appendWeak(ref1)
-        array.test(weakCount: 1, nonNilCount: 1, elements: [ref1!])
-        array.appendWeak(ref2)
-        array.test(weakCount: 2, nonNilCount: 2, elements: [ref1!, ref2!])
-        ref1 = nil
-        array.test(weakCount: 2, nonNilCount: 1, elements: [ref2!])
-        array.compact()
-        array.test(weakCount: 1, nonNilCount: 1, elements: [ref2!])
-        array.appendWeak(ref3)
-        array.test(weakCount: 2, nonNilCount: 2, elements: [ref2!, ref3!])
-        ref2 = nil
-        ref3 = nil
-        array.test(weakCount: 2, nonNilCount: 0, elements: [])
-        array.removeAll()
-        array.test(weakCount: 0, nonNilCount: 0, elements: [])
-    }
+    var ref1: Ref? = Ref(name: "str1")
+    var ref2: Ref? = Ref(name: "str2")
+    var ref3: Ref? = Ref(name: "str3")
+    var array = WeakArray<Ref>()
+    
+    array.assert(weakCount: 0, nonNilCount: 0, elements: [])
+    array.append(ref1)
+    array.assert(weakCount: 1, nonNilCount: 1, elements: [ref1!])
+    array.append(ref2)
+    array.assert(weakCount: 2, nonNilCount: 2, elements: [ref1!, ref2!])
+    ref1 = nil
+    array.assert(weakCount: 2, nonNilCount: 1, elements: [ref2!])
+    array.compact()
+    array.assert(weakCount: 1, nonNilCount: 1, elements: [ref2!])
+    array.append(ref3)
+    array.assert(weakCount: 2, nonNilCount: 2, elements: [ref2!, ref3!])
+    ref2 = nil
+    ref3 = nil
+    array.assert(weakCount: 2, nonNilCount: 0, elements: [])
+    array.weakElements.removeAll()
+    array.assert(weakCount: 0, nonNilCount: 0, elements: [])
+  }
 }
 
-private extension WeakArray where Element: Equatable {
+private extension WeakArray where Element == WeakTests.Ref {
+  
+  func assert(
+    weakCount: Int,
+    nonNilCount: Int,
+    elements expectedElements: [Element],
+    file: StaticString = #file,
+    line: UInt = #line)
+  {
+    let actualElements = self.strongElements()
     
-    func test(weakCount: Int, nonNilCount: Int, elements: [Element], file: StaticString = #file, line: UInt = #line) {
-        let arrayElements = self.elements
-        
-        XCTAssertEqual(weakCount, weakElements.count, file: file, line: line)
-        XCTAssertEqual(nonNilCount, arrayElements.count, file: file, line: line)
-        XCTAssertEqual(elements, arrayElements, file: file, line: line)
+    XCTAssertEqual(weakCount, weakElements.count, file: file, line: line)
+    XCTAssertEqual(nonNilCount, actualElements.count, file: file, line: line)
+    XCTAssertEqual(actualElements.count, expectedElements.count, file: file, line: line)
+    
+    for pair in zip(actualElements, expectedElements) {
+      XCTAssertTrue(pair.0 === pair.1, file: file, line: line)
     }
+  }
 }
