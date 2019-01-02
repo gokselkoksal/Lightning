de77849f361dab58b31298319343dde96213bbe3
Göksel Köksal <gokselkoksal@gmail.com>
2019-01-02 22:03:28 +0300

    Improve Protected tests

--- LightningTests/ComponentTests/ProtectedTests.swift
+++ LightningTests/ComponentTests/ProtectedTests.swift
@@ -10,23 +10,57 @@ import XCTest
 @testable import Lightning
 
 class ProtectedTests: XCTestCase {
-    
-    func testReadWrite() {
-        let list = Protected(["item1"])
-        XCTAssert(list.value == ["item1"])
-        list.value = ["item1", "item2"]
-        XCTAssert(list.value == ["item1", "item2"])
-        list.read { items in
-            XCTAssert(items == ["item1", "item2"])
-        }
-        list.write { items in
-            items.append("item3")
-            items.append("item4")
-            items = items.map { string in
-                let index = string.index(string.startIndex, offsetBy: 4)
-                return String(string[index..<string.endIndex])
-            }
-        }
-        XCTAssert(list.value == ["1", "2", "3", "4"])
+  
+  func testReadWrite_array() {
+    let protectedList = Protected(["item1"])
+    XCTAssert(protectedList.value == ["item1"])
+    protectedList.value = ["item1", "item2"]
+    XCTAssert(protectedList.value == ["item1", "item2"])
+    protectedList.read { items in
+      XCTAssert(items == ["item1", "item2"])
     }
+    protectedList.write { list in
+      list.append("item3")
+      list.append("item4")
+      list = list.map { string in
+        let index = string.index(string.startIndex, offsetBy: 4)
+        return String(string[index..<string.endIndex])
+      }
+    }
+    XCTAssert(protectedList.value == ["1", "2", "3", "4"])
+  }
+  
+  func testReadWrite_int() {
+    let protectedNumber = Protected(1)
+    protectedNumber.write { (number) in
+      number = 2
+    }
+    XCTAssertEqual(protectedNumber.value, 2)
+  }
+  
+  func testReadWrite_string() {
+    let protectedString = Protected("Goksel")
+    protectedString.write { (string) in
+      string = "Koksal"
+    }
+    XCTAssertEqual(protectedString.value, "Koksal")
+  }
+  
+  func testReadWrite_struct() {
+    
+    struct SomeStruct { // Local struct for testing purposes.
+      
+      var number: Int
+      
+      mutating func increment() {
+        number += 1
+      }
+    }
+    
+    let protectedSomeStruct = Protected(SomeStruct(number: 1))
+    protectedSomeStruct.write { (someStruct) in
+      someStruct.increment()
+    }
+    XCTAssertEqual(protectedSomeStruct.value.number, 2)
+  }
 }
