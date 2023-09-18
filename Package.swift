c39ab441cc88e0a377347ed371bfa1256e170a1b
Egemen Türk <egementurk-mobillium@Egemens-MacBook-Pro.local>
2023-09-18 23:28:42 +0300

    add supported platforms

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "Lightning",
+    platforms: [.macOS(.v10_12), .iOS(.v10), .tvOS(.v10), .watchOS(.v3)],
     products: [
         .library(
             name: "Lightning",
